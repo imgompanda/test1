2af80d3c4eb0ab3e25982b6d05232171a5469c0f
Gompanda <browoo051@gmail.com>
2025-04-17 09:13:15 +0900

    ㅇㅇ

---
+++ app/src/main/java/com/example/mydeviceinfo/presentation/ui/main/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.mydeviceinfo.presentation.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: LiveData 및 비즈니스 로직 추가
+    // 예시: 기기 정보를 담을 LiveData
+    // val deviceInfoLiveData = MutableLiveData<DeviceInfo>()
+} 
